09d6d24e5b61a271dbcdf0043b0562795fffe063
AgnesTachyon-feet <kuysasa007@gmail.com>
2026-05-02 17:44:33 +0700

    c

--- main.go
+++ main.go
@@ -4,8 +4,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author`
+}
+
+var books []Book
+
 func main() {
 	app := fiber.New()
+
+	books = append(books, Book{ID: 1, Tile: "1984", Author: "George Orwell"})
+	books = append(books, Book{ID: 2, Tile: "The Great Gatsby", Author: "F. Soctt Fitzgerald"})
 
 	app.Get("/hello", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World")
